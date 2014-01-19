6a75cec1727907384a3f0ff75ccedeec5fd59b06
Ryan O'Leary <rjoleary@rogers.com>
2014-01-18 20:11:52 -0500

    Fix formatting mistake

--- project-euler/problem4.cpp
+++ project-euler/problem4.cpp
@@ -6,35 +6,35 @@ int reverse(int value);
 int main()
 {
     // Test all possible combinations of a*b and keep the largest palindrome.
-	int largestProduct = 0;
-	for (int a = 999; a >= 0; a--)
-	{
-		for (int b = a; b >= 0; b--)
-		{
-			int product = a * b;
-			
-			// Since b iterates in reverse, a significant number of combinations
-			//  do not need to be checked.
-			if (product < largestProduct)
-			    break;
-			    
-			if (reverse(product) == product)
-				largestProduct = std::max(product, largestProduct);
-		}
-	}
+    int largestProduct = 0;
+    for (int a = 999; a >= 0; a--)
+    {
+        for (int b = a; b >= 0; b--)
+        {
+            int product = a * b;
 
-	std::cout << largestProduct << '\n';
+            // Since b iterates in reverse, a significant number of combinations
+            //  do not need to be checked.
+            if (product < largestProduct)
+                break;
+
+            if (reverse(product) == product)
+                largestProduct = std::max(product, largestProduct);
+        }
+    }
+
+    std::cout << largestProduct << '\n';
 }
 
 int reverse(int value)
 {
-	int reverse = 0;
-	while (value > 0)
-	{
-	    reverse *= 10;
-	    reverse += value % 10;
-		value = value / 10;
-	}
+    int reverse = 0;
+    while (value > 0)
+    {
+        reverse *= 10;
+        reverse += value % 10;
+        value = value / 10;
+    }
 
-	return reverse;
+    return reverse;
 }
